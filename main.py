1022da69a2cda3e94539a0c2fe8cdeea38d9bea5
vilion <takabatakekoichi@gmail.com>
2017-11-20 23:24:28 +0900

    fix error

--- main.py
+++ main.py
@@ -135,8 +135,10 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
     for epoch in epochs:
         print("EPOCH {} ...".format(epoch+1))
         for imate, label in get_batches_fn(batch_size):
-            _, loss = sess.run([train_op, cross_entropy_loss],
-                    feed_dict={input_image: image, correct_label: label, keep_prob: 0.5, learning_rate=0.0009})
+            _, loss = sess.run(
+                [train_op, cross_entropy_loss],
+                feed_dict={input_image: image, correct_label: label, keep_prob: 0.5, learning_rate:0.0009}
+            )
             print("Loss: = {:.3f}".format(loss))
         print()
 
