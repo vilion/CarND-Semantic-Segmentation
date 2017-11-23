e82805b21339df67e53bcdd1d30385b2bc6c06d0
vilion <takabatakekoichi@gmail.com>
2017-11-23 17:33:36 +0900

    garden

--- main.py
+++ main.py
@@ -134,7 +134,7 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
 
     for epoch in range(epochs):
         print("EPOCH {} ...".format(epoch+1))
-        for imate, label in get_batches_fn(batch_size):
+        for image, label in get_batches_fn(batch_size):
             _, loss = sess.run(
                 [train_op, cross_entropy_loss],
                 feed_dict={input_image: image, correct_label: label, keep_prob: 0.5, learning_rate:0.0009}
